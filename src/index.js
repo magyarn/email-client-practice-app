6563ab27bd2423a6f4bae6bdb9b0cc35f7d0b9d6
Nathan Magyar <magyarn@umich.edu>
2018-01-23 15:02:18 -0500

    Refactor Task 1

--- src/index.js
+++ src/index.js
@@ -2,38 +2,21 @@
  * @flow
  */
 import * as emails from './store/emails.json';
+import { formattedMessages } from './store';
 
 // example of generating HTML with js
-function renderNewMessage(sender, timestamp, subject, snippet) {
+function renderNewMessage(newMessage) {
   const sidebar = document.querySelector('.email-list');
-  const newMessage = document.createElement('li');
-  newMessage.innerHTML = `<button class="email-item" type="button">
+  const newMessageLI = document.createElement('li');
+  newMessageLI.innerHTML = `<button class="email-item" type="button">
                             <div class="sender-details">
-                              <p>${sender}</p>
-                              <span>${timestamp}</span>
+                              <p>${newMessage.sender}</p>
+                              <span>${newMessage.date}</span>
                             </div>
-                            <p class="email-subject">${subject}</p>
-                            <p>${snippet}</p>
+                            <p class="email-subject">${newMessage.subject}</p>
+                            <p>${newMessage.snippet}</p>
                           </button>`;
-  sidebar.appendChild(newMessage);
-}
-
-
-function formatTimestamp(datestring) {
-  const d = new Date(datestring);
-  const month = d.getMonth() + 1;
-  const day = d.getDay();
-  const year = String(d.getFullYear()).substring(2);
-  const timestamp = `${month}/${day}/${year}`;
-  return timestamp;
-}
-
-function formatSubject (subject) {
-  return subject.length > 33 ? subject.substring(0, 33) + '...' : subject;
-}
-
-function formatSnippet (snippet) {
-  return snippet.length > 77 ? snippet.substring(0, 77) + '...' : snippet;
+  sidebar.appendChild(newMessageLI);
 }
 
 function renderSidebar() {
@@ -44,16 +27,8 @@ function renderSidebar() {
   const container = document.querySelector('.email-list');
   if (container != null) container.innerHTML += sidebarContents;
 
-  const newMessageIDs = emails.mailboxes.INBOX.threadIds;
-  const allMessages = emails.messages;
-
-  for (const m of newMessageIDs) {
-    const payloadHeaders = allMessages[m].payload.headers;
-    const snippet = formatSnippet(allMessages[m].snippet);
-    const timestamp = formatTimestamp(payloadHeaders[0].value);
-    const sender = payloadHeaders[1].value.match(/([A-Z])\w+/g).join(' ');
-    const subject = formatSubject(payloadHeaders[3].value);
-    renderNewMessage(sender, timestamp, subject, snippet);
+  for (const m of formattedMessages) {
+    renderNewMessage(m);
   }
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -9,17 +9,62 @@ import type { Data, Message } from './types';
 
 const store: Data = require('./emails.json');
 
-/**
- * You might find it useful to implement some functions that make accessing
- * deeply nested attributes of Messages or Threads...
- */
-
-// For example, this returns the subject of a given message if it is defined
-// by a header in the message payload
-export function getMessageSubject(message: Message): ?string {
-  const { headers } = message.payload;
-  const subjectHeader = headers.find(header => header.name === 'Subject');
-  return subjectHeader && subjectHeader.value;
+function getNewMessageIds() {
+  return store.mailboxes.INBOX.threadIds;
 }
 
-export default store;
+function getNewMessages(newMessageIds) {
+  let newMessages = [];
+  for (let id = 0; id < newMessageIds.length; id += 1) {
+    const rawMessageObj = store.messages[newMessageIds[id]];
+    const refinedMessageObj = {
+      Snippet: rawMessageObj.snippet,
+      Date: '',
+      From: '',
+      To: '',
+      Subject: '',
+    };
+    for (let header of rawMessageObj.payload.headers) {
+      refinedMessageObj[header.name] = header.value;
+    };
+    newMessages.push(refinedMessageObj);
+  }
+  return newMessages;
+}
+
+function formatNewMessage(newMessage) {
+  const formattedRecipients = newMessage.To;
+  const formattedDate = formatTimestamp(newMessage.Date);
+  const formattedSender = newMessage.From.match(/([A-Z])\w+/g).join(' ');
+  const formattedSubject = formatSubject(newMessage.Subject);
+  const formattedSnippet = formatSnippet(newMessage.Snippet);
+  const formattedMessage = {
+    recipients: formattedRecipients,
+    sender: formattedSender,
+    subject: formattedSubject,
+    snippet: formattedSnippet,
+    date: formattedDate
+  }
+  return formattedMessage;
+}
+
+function formatTimestamp(datestring) {
+  const d = new Date(datestring);
+  const month = d.getMonth() + 1;
+  const day = d.getDay();
+  const year = String(d.getFullYear()).substring(2);
+  const timestamp = `${month}/${day}/${year}`;
+  return timestamp;
+}
+
+function formatSubject (subject) {
+  return subject.length > 33 ? subject.substring(0, 33) + '...' : subject;
+}
+
+function formatSnippet (snippet) {
+  return snippet.length > 77 ? snippet.substring(0, 77) + '...' : snippet;
+}
+
+const newMessageIds = getNewMessageIds();
+const newMessages = getNewMessages(newMessageIds);
+export const formattedMessages = newMessages.map(newMessage => formatNewMessage(newMessage));
