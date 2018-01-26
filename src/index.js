f190a02de8cb6ddc01f3d65b708ca2292734d29c
Nathan Magyar <magyarn@umich.edu>
2018-01-26 08:41:19 -0500

    Restructure sidebar rendering functions and format date object

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
 /**
  * @flow
  */
-import * as emails from './store/emails.json';
-import { formattedMessages } from './store';
-
-// example of generating HTML with js
-function renderNewMessage(newMessage) {
-  const sidebar = document.querySelector('.email-list');
-  const newMessageLI = document.createElement('li');
-  newMessageLI.innerHTML = `<button class="email-item" type="button">
-                            <div class="sender-details">
-                              <p>${newMessage.sender}</p>
-                              <span>${newMessage.date}</span>
-                            </div>
-                            <p class="email-subject">${newMessage.subject}</p>
-                            <p>${newMessage.snippet}</p>
-                          </button>`;
-  sidebar.appendChild(newMessageLI);
-}
+import { getThreads } from './store';
 
 function renderSidebar() {
+  const messageLIs = getThreads().reduce((html, thread) => `${html} <li>
+  <button class="email-item" type="button">
+      <div class="sender-details">
+        <p>${thread.sender}</p>
+        <span>${thread.timestamp}</span>
+      </div>
+      <p class="email-subject">${thread.subject}</p>
+      <p>${thread.snippet}</p>
+    </button>
+  </li>`, '');
   const sidebarContents = `
-    <ul class="email-list"></ul>
+    <ul class="email-list">${messageLIs}</ul>
   `;
-
   const container = document.querySelector('.email-list');
   if (container != null) container.innerHTML += sidebarContents;
-
-  for (const m of formattedMessages) {
-    renderNewMessage(m);
-  }
 }
 
 renderSidebar();
--- src/store/index.js
+++ src/store/index.js
@@ -9,62 +9,117 @@ import type { Data, Message } from './types';
 
 const store: Data = require('./emails.json');
 
-function getNewMessageIds() {
-  return store.mailboxes.INBOX.threadIds;
-}
-
-function getNewMessages(newMessageIds) {
-  let newMessages = [];
-  for (let id = 0; id < newMessageIds.length; id += 1) {
-    const rawMessageObj = store.messages[newMessageIds[id]];
-    const refinedMessageObj = {
-      Snippet: rawMessageObj.snippet,
-      Date: '',
-      From: '',
-      To: '',
-      Subject: '',
-    };
-    for (let header of rawMessageObj.payload.headers) {
-      refinedMessageObj[header.name] = header.value;
-    };
-    newMessages.push(refinedMessageObj);
+function formatTimestamp(timestamp) {
+  const now = Date.now();
+  const midnightToday = new Date().setUTCHours(0, 0, 0, 0);
+  const timestampMilliseconds = Date.parse(timestamp);
+  const justDate = new Date(timestamp).toLocaleDateString();
+  const justTime = new Date(timestamp).toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  if (timestampMilliseconds < now && timestampMilliseconds > midnightToday) {
+    return justTime;
   }
-  return newMessages;
+  return justDate;
 }
 
-function formatNewMessage(newMessage) {
-  const formattedRecipients = newMessage.To;
-  const formattedDate = formatTimestamp(newMessage.Date);
-  const formattedSender = newMessage.From.match(/([A-Z])\w+/g).join(' ');
-  const formattedSubject = formatSubject(newMessage.Subject);
-  const formattedSnippet = formatSnippet(newMessage.Snippet);
-  const formattedMessage = {
-    recipients: formattedRecipients,
-    sender: formattedSender,
-    subject: formattedSubject,
-    snippet: formattedSnippet,
-    date: formattedDate
+function formatSender(sender) {
+  return sender.match(/.+?(?=\\u003)/)[0];
+}
+
+function getHeaderValue(headers, name) {
+  const { value } = headers.filter(header => header.name === name)[0];
+  if (name === 'Date') {
+    return formatTimestamp(value);
   }
-  return formattedMessage;
+  if (name === 'From') {
+    return formatSender(value);
+  }
+  return value;
 }
 
-function formatTimestamp(datestring) {
-  const d = new Date(datestring);
-  const month = d.getMonth() + 1;
-  const day = d.getDay();
-  const year = String(d.getFullYear()).substring(2);
-  const timestamp = `${month}/${day}/${year}`;
-  return timestamp;
+export function getThreads() {
+  const { mailboxes, threads } = store;
+  const inbox = mailboxes.INBOX;
+  console.log(threads);
+  const threadObjects = inbox.threadIds.map((id) => {
+    const [newestMessage] = threads[id].messages.slice(-1);
+    const message = store.messages[newestMessage.id];
+    const { headers } = message.payload;
+    const sender = getHeaderValue(headers, 'From');
+    const timestamp = getHeaderValue(headers, 'Date');
+    const subject = getHeaderValue(headers, 'Subject') || '(No Subject)';
+    const { snippet } = message;
+    return {
+      sender,
+      timestamp,
+      subject,
+      snippet,
+    };
+  });
+  return threadObjects;
 }
 
-function formatSubject (subject) {
-  return subject.length > 33 ? subject.substring(0, 33) + '...' : subject;
-}
+// function getNewMessageIds() {
+//   return store.mailboxes.INBOX.threadIds;
+// }
+//
+// function getNewMessages(newMessageIds) {
+//   const newMessages = [];
+//   for (let id = 0; id < newMessageIds.length; id += 1) {
+//     const rawMessageObj = store.messages[newMessageIds[id]];
+//     const refinedMessageObj = {
+//       Snippet: rawMessageObj.snippet,
+//       Date: '',
+//       From: '',
+//       To: '',
+//       Subject: '',
+//     };
+//     for (const header of rawMessageObj.payload.headers) {
+//       refinedMessageObj[header.name] = header.value;
+//     }
+//     newMessages.push(refinedMessageObj);
+//   }
+//   return newMessages;
+// }
+//
+// function formatTimestamp(datestring) {
+//   const d = new Date(datestring);
+//   const month = d.getMonth() + 1;
+//   const day = d.getDay();
+//   const year = String(d.getFullYear()).substring(2);
+//   const timestamp = `${month}/${day}/${year}`;
+//   return timestamp;
+// }
+//
+// function formatSubject (subject) {
+//   return subject.length > 25 ? subject.substring(0, 25) + '...' : subject;
+// }
+//
+// function formatSnippet (snippet) {
+//   return snippet.length > 70 ? snippet.substring(0, 70) + '...' : snippet;
+// }
+//
+// function formatNewMessage(newMessage) {
+//   const formattedRecipients = newMessage.To;
+//   const formattedDate = formatTimestamp(newMessage.Date);
+//   const formattedSender = newMessage.From.match(/([A-Z])\w+/g).join(' ');
+//   const formattedSubject = formatSubject(newMessage.Subject);
+//   const formattedSnippet = formatSnippet(newMessage.Snippet);
+//   const formattedMessage = {
+//     recipients: formattedRecipients,
+//     sender: formattedSender,
+//     subject: formattedSubject,
+//     snippet: formattedSnippet,
+//     date: formattedDate
+//   }
+//   return formattedMessage;
+// }
+//
+// const newMessageIds = getNewMessageIds();
+// const newMessages = getNewMessages(newMessageIds);
+// export const formattedMessages = newMessages.map(newMessage =>
+// formatNewMessage(newMessage));
 
-function formatSnippet (snippet) {
-  return snippet.length > 77 ? snippet.substring(0, 77) + '...' : snippet;
-}
-
-const newMessageIds = getNewMessageIds();
-const newMessages = getNewMessages(newMessageIds);
-export const formattedMessages = newMessages.map(newMessage => formatNewMessage(newMessage));
+export default store;
