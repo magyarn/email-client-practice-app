931a5abc3f57f85478e086a3737d7d9c07f39a6c
Nathan Magyar <magyarn@umich.edu>
2018-01-26 08:55:23 -0500

    Delete commented out code

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,4 @@
 /**
- * The data store is currently a JSON file, but its schema mirrors the Gmail
- * API so we can feed it from your own inbox at a later date.
- *
  * @flow
  */
 
@@ -42,7 +39,6 @@ function getHeaderValue(headers, name) {
 export function getThreads() {
   const { mailboxes, threads } = store;
   const inbox = mailboxes.INBOX;
-  console.log(threads);
   const threadObjects = inbox.threadIds.map((id) => {
     const [newestMessage] = threads[id].messages.slice(-1);
     const message = store.messages[newestMessage.id];
@@ -60,66 +56,5 @@ export function getThreads() {
   });
   return threadObjects;
 }
-
-// function getNewMessageIds() {
-//   return store.mailboxes.INBOX.threadIds;
-// }
-//
-// function getNewMessages(newMessageIds) {
-//   const newMessages = [];
-//   for (let id = 0; id < newMessageIds.length; id += 1) {
-//     const rawMessageObj = store.messages[newMessageIds[id]];
-//     const refinedMessageObj = {
-//       Snippet: rawMessageObj.snippet,
-//       Date: '',
-//       From: '',
-//       To: '',
-//       Subject: '',
-//     };
-//     for (const header of rawMessageObj.payload.headers) {
-//       refinedMessageObj[header.name] = header.value;
-//     }
-//     newMessages.push(refinedMessageObj);
-//   }
-//   return newMessages;
-// }
-//
-// function formatTimestamp(datestring) {
-//   const d = new Date(datestring);
-//   const month = d.getMonth() + 1;
-//   const day = d.getDay();
-//   const year = String(d.getFullYear()).substring(2);
-//   const timestamp = `${month}/${day}/${year}`;
-//   return timestamp;
-// }
-//
-// function formatSubject (subject) {
-//   return subject.length > 25 ? subject.substring(0, 25) + '...' : subject;
-// }
-//
-// function formatSnippet (snippet) {
-//   return snippet.length > 70 ? snippet.substring(0, 70) + '...' : snippet;
-// }
-//
-// function formatNewMessage(newMessage) {
-//   const formattedRecipients = newMessage.To;
-//   const formattedDate = formatTimestamp(newMessage.Date);
-//   const formattedSender = newMessage.From.match(/([A-Z])\w+/g).join(' ');
-//   const formattedSubject = formatSubject(newMessage.Subject);
-//   const formattedSnippet = formatSnippet(newMessage.Snippet);
-//   const formattedMessage = {
-//     recipients: formattedRecipients,
-//     sender: formattedSender,
-//     subject: formattedSubject,
-//     snippet: formattedSnippet,
-//     date: formattedDate
-//   }
-//   return formattedMessage;
-// }
-//
-// const newMessageIds = getNewMessageIds();
-// const newMessages = getNewMessages(newMessageIds);
-// export const formattedMessages = newMessages.map(newMessage =>
-// formatNewMessage(newMessage));
 
 export default store;
